8adb273d34b9721e4917140293b53b69051b005d
VovaVindar <volodimir.budkevych@gmail.com>
2024-06-07 22:13:27 -0700

    Finished textarea; Added auto rotation for 3js; Styled scrollbars;

--- components/ui/form/textarea.jsx
+++ components/ui/form/textarea.jsx
@@ -1,4 +1,5 @@
 import styles from "./form.module.css";
+import Image from "next/image";
 
 const Textarea = ({
   label,
@@ -13,6 +14,12 @@ const Textarea = ({
       <label htmlFor={name} className={`text-mono-1`}>
         {label}
       </label>
+      <div
+        className={`${styles["input-container__textarea-resizer-bg"]}`}
+      ></div>
+      <div className={`${styles["input-container__textarea-resizer"]}`}>
+        <Image src="/img/icons/resize.svg" alt="Resize" fill sizes="20px" />
+      </div>
       <textarea
         id={name}
         className={`${styles["input-container__textarea"]} text-body-2 surface-color-tertiary`}
@@ -22,6 +29,7 @@ const Textarea = ({
         aria-required={ariaRequired}
         maxLength="2999"
         onChange={handleChange}
+        rows={8}
       ></textarea>
     </div>
   );
