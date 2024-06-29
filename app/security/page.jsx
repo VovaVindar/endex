b0314f0fdde89f719f90a855e1f51e58f896b83d
VovaVindar <volodimir.budkevych@gmail.com>
2024-06-29 20:13:59 +0200

    Started security

---
+++ app/security/page.jsx
@@ -0,0 +1,34 @@
+import styles from "./security.module.css";
+import Dialog from "@/components/ui/dialog";
+
+export const metadata = {
+  title: "Security & Privacy",
+};
+
+export default function Careers() {
+  return (
+    <main style={{ padding: "0 var(--page-padding)" }}>
+      <div className={`${styles["hero-container"]}`}>
+        <h1 className={`${styles["careers-heading"]} text-heading-1`}>
+          Security & Privacy
+        </h1>
+        <p className={`${styles["careers-description"]} text-body-1`}>
+          Your data is always secure with Endex. We are committed to upholding
+          the most rigorous data safeguards and standards, ensuring your
+          information remains safe.
+        </p>
+        <div className={`${styles["hero-btn-container"]}`}>
+          <Dialog
+            trigger={<Button variant="primary">Contact Sales</Button>}
+            content={<Form addHeadline={true} headline="Contact Sales" />}
+          />
+          <Link href="/custom-deployment">
+            <Button variant="secondary" tabIndex="-1">
+              Custom Deployment
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
+}
